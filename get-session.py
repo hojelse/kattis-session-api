abd9ab9bb7f687d807dcc5e65fb7a192318284a3
Kristoffer Højelse <info@hojelse.com>
2021-01-09 22:41:59 +0100

    Create get-session.py

---
+++ get-session.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+import os
+import re
+import sys
+import webbrowser
+
+import requests
+import requests.exceptions
+
+# Python 2/3 compatibility
+if sys.version_info[0] >= 3:
+    import configparser
+else:
+    # Python 2, import modules with Python 3 names
+    import ConfigParser as configparser
+
+# End Python 2/3 compatibility
+
+_DEFAULT_CONFIG = '/usr/local/etc/kattisrc'
+
+_HEADERS = {'User-Agent': 'kattis-session-api'}
+
+
+class ConfigError(Exception):
+    pass
+
+
+def get_url(cfg, option, default):
+    if cfg.has_option('kattis', option):
+        return cfg.get('kattis', option)
+    else:
+        return 'https://%s/%s' % (cfg.get('kattis', 'hostname'), default)
+
+
+def get_config():
+    """Returns a ConfigParser object for the .kattisrc file(s)
+    """
+    cfg = configparser.ConfigParser()
+    if os.path.exists(_DEFAULT_CONFIG):
+        cfg.read(_DEFAULT_CONFIG)
+
+    if not cfg.read([os.path.join(os.path.expanduser("~"), '.kattisrc'),
+                     os.path.join(os.path.dirname(sys.argv[0]), '.kattisrc')]):
+        raise ConfigError('''\
+I failed to read in a config file from your home directory or from the
+same directory as this script. To download a .kattisrc file please visit
+https://<kattis>/download/kattisrc
+
+The file should look something like this:
+[user]
+username: yourusername
+token: *********
+
+[kattis]
+hostname: <kattis>
+loginurl: https://<kattis>/login
+submissionurl: https://<kattis>/submit
+submissionsurl: https://<kattis>/submissions''')
+    return cfg
+
+def login(login_url, username, password=None, token=None):
+    """Log in to Kattis.
+
+    At least one of password or token needs to be provided.
+
+    Returns a requests.Response with cookies needed to be able to submit
+    """
+    login_args = {'user': username, 'script': 'true'}
+    if password:
+        login_args['password'] = password
+    if token:
+        login_args['token'] = token
+
+    return requests.post(login_url, data=login_args, headers=_HEADERS)
+
+
+def login_from_config(cfg):
+    """Log in to Kattis using the access information in a kattisrc file
+
+    Returns a requests.Response with cookies needed to be able to submit
+    """
+    username = cfg.get('user', 'username')
+    password = token = None
+    try:
+        password = cfg.get('user', 'password')
+    except configparser.NoOptionError:
+        pass
+    try:
+        token = cfg.get('user', 'token')
+    except configparser.NoOptionError:
+        pass
+    if password is None and token is None:
+        raise ConfigError('''\
+Your .kattisrc file appears corrupted. It must provide a token (or a
+KATTIS password).
+
+Please download a new .kattisrc file''')
+
+    loginurl = get_url(cfg, 'loginurl', 'login')
+    return login(loginurl, username, password, token)
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='kattis', description='hello :)')
+#     parser.add_argument('-p', '--problem',
+#                         help=''''Which problem to submit to.
+# Overrides default guess (first part of first filename)''')
+   
+
+    args = parser.parse_args()
+
+    try:
+        cfg = get_config()
+    except ConfigError as exc:
+        print(exc)
+        sys.exit(1)
+
+    try:
+        login_reply = login_from_config(cfg)
+        print(login_reply)
+    except ConfigError as exc:
+        print(exc)
+        sys.exit(1)
+    except requests.exceptions.RequestException as err:
+        print('Login connection failed:', err)
+        sys.exit(1)
+
+    if not login_reply.status_code == 200:
+        print('Login failed.')
+        if login_reply.status_code == 403:
+            print('Incorrect username or password/token (403)')
+        elif login_reply.status_code == 404:
+            print('Incorrect login URL (404)')
+        else:
+            print('Status code:', login_reply.status_code)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
